586d9bed2d92d01d2d8c64848ec0a524f7d05474
a.pahomov <a.pahomov@smartway.today>
2025-07-02 15:57:11 +0300

    send image first try

--- methods/telegramapi/models.go
+++ methods/telegramapi/models.go
@@ -4,3 +4,8 @@ type SendMessageRequest struct {
 	ChatId int64  `json:"chat_id"`
 	Text   string `json:"text"`
 }
+
+type SendPhotoRequest struct {
+	ChatId int64  `json:"chat_id"`
+	Photo  string `json:"photo"`
+}
