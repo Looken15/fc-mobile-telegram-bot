b480e07d7adeecce0b5b91199b8493eaca40e305
a.pahomov <a.pahomov@smartway.today>
2025-07-02 18:00:44 +0300

    main logic

--- methods/telegramapi/models.go
+++ methods/telegramapi/models.go
@@ -3,10 +3,33 @@ package telegramapi
 type SendMessageRequest struct {
 	ChatId int64  `json:"chat_id"`
 	Text   string `json:"text"`
+	//ReplyKeyboardMarkup  ReplyKeyboardMarkup  `json:"reply_markup"`
+	InlineKeyboardMarkup InlineKeyboardMarkup `json:"reply_markup"`
+	ParseMode            string               `json:"parse_mode"`
 }
 
 type SendPhotoRequest struct {
-	ChatId  int64  `json:"chat_id"`
-	Photo   string `json:"photo"`
-	Caption string `json:"caption"`
+	ChatId               int64                `json:"chat_id"`
+	Caption              string               `json:"caption"`
+	InlineKeyboardMarkup InlineKeyboardMarkup `json:"reply_markup"`
+	ParseMode            string               `json:"parse_mode"`
+	Photo                string               `json:"photo"`
+}
+
+type InlineKeyboardMarkup struct {
+	Keyboard [][]InlineKeyboardButton `json:"inline_keyboard"`
+}
+
+type InlineKeyboardButton struct {
+	Text         string `json:"text"`
+	CallbackData string `json:"callback_data"`
+}
+
+type ReplyKeyboardMarkup struct {
+	Keyboard       [][]KeyboardButton `json:"keyboard"`
+	ResizeKeyboard bool               `json:"resize_keyboard"`
+}
+
+type KeyboardButton struct {
+	Text string `json:"text"`
 }
