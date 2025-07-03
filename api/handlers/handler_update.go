3cf825283e81dfa1fca48b7e5b630c8b6ddd853f
a.pahomov <a.pahomov@smartway.today>
2025-07-03 18:53:11 +0300

    !!temp!!

--- api/handlers/handler_update.go
+++ api/handlers/handler_update.go
@@ -11,15 +11,29 @@ import (
 
 func UpdateHandler(telegramService telegramservice.TelegramService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		params := models.TelegramUpdate{}
+		/*params := models.TelegramUpdate{}
 		err := json.NewDecoder(r.Body).Decode(&params)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}*/
+
+		var rawBody interface{}
+		err := json.NewDecoder(r.Body).Decode(&rawBody)
 		if err != nil {
 			log.Println(err)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
-		_ = utils.PrintJSON(params)
+		_ = utils.PrintJSON(rawBody)
+
+		params, ok := rawBody.(models.TelegramUpdate)
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
 
 		err = telegramService.Response(params)
 		if err != nil {
