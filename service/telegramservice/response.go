4721b43867e86854f64e3b010a9a1c52d1881593
a.pahomov <a.pahomov@smartway.today>
2026-02-02 14:17:34 +0300

    date change

--- service/telegramservice/response.go
+++ service/telegramservice/response.go
@@ -17,7 +17,7 @@ const (
 	_imagePathJPG  = "./images/%s.jpg"
 	_imagePathPNG  = "./images/%s.png"
 
-	_lastUpdateDate = "26 января, 2025"
+	_lastUpdateDate = "02 февраля, 2026"
 
 	_sendPhotoCaption = "<b>ТОП-10 %s в FC Mobile</b>\n\nПоследнее обновление:\n%s\n\n<a href=\"http://t.me/KaramaFC\">KARAMA | FC MOBILE 26</a>"
 
