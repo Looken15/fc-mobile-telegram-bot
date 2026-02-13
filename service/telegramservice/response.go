0028496b6d50640600f9f7c284bd663bfe682df7
a.pahomov <a.pahomov@smartway.today>
2026-02-13 19:42:48 +0300

    enable just for me
    fix
    
    fix

--- service/telegramservice/response.go
+++ service/telegramservice/response.go
@@ -168,6 +168,14 @@ func (s *TelegramService) sendPositionsMessage() error {
 }
 
 func (s *TelegramService) Response(params models.TelegramUpdate) (err error) {
+	if params.Message != nil && params.Message.From.Username != "looken15" {
+		return
+	}
+
+	if params.CallbackQuery != nil && params.CallbackQuery.From.Username != "looken15" {
+		return
+	}
+
 	if params.Message != nil {
 		s.fillParams(params.Message.From.ID, params.Message.MessageID, params.Message.Chat.ID, params.Message.From.Username)
 	}
