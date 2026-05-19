efd77d45111cfb48a5e3dda5789c0bc7214a26ff
a.pahomov <a.pahomov@smartway.today>
2026-05-19 16:38:53 +0300

    remove basement

--- methods/telegramapi/get_chat_member.go
+++ methods/telegramapi/get_chat_member.go
@@ -29,14 +29,14 @@ func (c *TelegramApi) CheckIfUserSub(userId int64) (bool, error) {
 		return false, nil
 	}
 
-	basementRes, err := c.GetChatMember(GetChatMemberRequest{UserId: userId, ChatId: _basementChatId})
+	/*basementRes, err := c.GetChatMember(GetChatMemberRequest{UserId: userId, ChatId: _basementChatId})
 	if err != nil {
 		return false, err
 	}
 
 	if !lo.Contains(_membersArray, basementRes.Result.Status) {
 		return false, nil
-	}
+	}*/
 
 	return true, nil
 }
